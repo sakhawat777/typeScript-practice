2397538ef68720924cb21aa690686357aad2b06f
sakhawat777 <biplobbms@gmail.com>
2022-02-22 21:07:46 +0600

    Variable, function, array and object practice done

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 "use strict";
+// variable or constant
 const country = "I love Bangladesh";
 console.log(country);
+// function
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(12, 10));
+// array
+let fruits = ["apple", "orange", "banana"];
+fruits.push("grape");
+console.log(fruits);
+// object
+let person = {
+    name: "Mashrafi",
+    age: 35,
+    isCaptrain: true,
+};
+person.name = "Tamim";
+console.log(person);
