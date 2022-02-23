fc38d282d795926b3929c676909dd80f669150b1
sakhawat777 <biplobbms@gmail.com>
2022-02-23 09:50:19 +0600

    Updated TS

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ let person = {
 };
 person.name = "Tamim";
 console.log(person);
+let a;
+a = "Sakhawat";
+let b;
+b = 100;
+let s = [];
+s.push("Biplob");
+let z;
+z = 500;
+let c;
+// any type variable
+let e;
+// or
+let d;
+// any type or dynamic type
+let f;
+f = {
+    name: "Tina",
+    roll: 1,
+};
+// Function type
+let myFunc;
+myFunc = () => {
+    console.log("Function");
+};
+// here c is a optional parameter and return string
+const arrayFunction = (a, b, c) => {
+    console.log(`${a}  ${b}`);
+    return (a + b);
+};
+arrayFunction("Hello", "World");
+const userDetails = (id, user) => {
+    console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`Hello ${user.age > 50 ? "Sir" : "Mr."}  ${user.name}`);
+};
+// Function Signatures
+let myFun;
+let add;
+add = (p, k) => {
+    return p + k;
+};
+console.log(add(2, 3));
+let userDetails2;
+userDetails2 = (id, user) => {
+    console.log(`id: ${id} user: ${user.name}`);
+};
+userDetails2(1, {
+    name: "Tina",
+    age: 20
+});
--- script.ts
+++ script.ts
@@ -19,3 +19,70 @@ let person = {
 };
 person.name = "Tamim";
 console.log(person);
+
+let a: string;
+a = "Sakhawat";
+let b: number;
+b = 100;
+let s:(string | number) [] = [];
+s.push("Biplob");
+let z: number | string;
+z = 500;
+let c: object;
+// any type variable
+let e;
+// or
+let d: any;
+// any type or dynamic type
+let f: {
+    name: any,
+    roll: any,
+
+};
+f = {
+    name: "Tina",
+    roll: 1,
+}
+// Function type
+ let myFunc: Function;
+ myFunc = () => {
+     console.log("Function");
+ }
+ // here c is a optional parameter and return string
+ const arrayFunction = (a: string, b: string, c?: string): string => {
+     console.log(`${a}  ${b}`);
+     return (a + b);
+ }
+arrayFunction("Hello", "World");
+
+// Type Aliases or custom type
+type stringOrNum = string | number;
+type userType = {name: string, age: number};
+const userDetails = (id: stringOrNum, user: userType) => {
+    console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`)
+}
+const sayHello = (user: userType) => {
+    console.log(`Hello ${user.age > 50? "Sir": "Mr."}  ${user.name}`)
+}
+// Function Signatures
+let myFun: (x: string, y: string) => void;
+let add: (i: number , j: number) => number;
+
+ add = (p: number, k: number) => {
+    return p + k;
+}
+console.log(add(2,3));
+let userDetails2: (id: number | string, userInfo: {
+    name: string, 
+    age: number
+}) => void;
+userDetails2 = (id: number | string,  user: {
+    name: string, 
+    age: number
+} ) => {
+console.log(`id: ${id} user: ${ user.name}`)
+}
+userDetails2(1,{
+    name: "Tina",
+    age: 20
+});
